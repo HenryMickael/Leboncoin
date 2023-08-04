922c3375672d68f29479f7661a9d13106063bc44
Your Name <henry.mickael1@gmail.com>
2023-08-04 16:20:45 +0200

    test pour backend

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,9 @@ mongoose
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.use(express.static("../frontend/build"));
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
