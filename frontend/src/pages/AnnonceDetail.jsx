a6181638b92240629f43b2535bb934d103d9dff3
Your Name <henry.mickael1@gmail.com>
2023-08-04 15:35:56 +0200

    save avant deploiment

--- frontend/src/pages/AnnonceDetail.jsx
+++ frontend/src/pages/AnnonceDetail.jsx
@@ -84,15 +84,14 @@ const AnnonceDetail = () => {
     fetchAnnonceDetail();
   }, [fetchAnnonceDetail]);
 
-  const handleDelete = async (id) => {
-    try {
-      await axios.delete(`http://localhost:4000/api/depotVehicule/${id}`);
-      // Rediriger vers la liste des annonces après la suppression réussie
-      window.location.href = "/article";
-    } catch (error) {
-      console.error("Erreur lors de la suppression de l'annonce:", error);
-    }
-  };
+  // const handleDelete = async (id) => {
+  //   try {
+  //     await axios.delete(`http://localhost:4000/api/depotVehicule/${id}`);
+  //     window.location.href = "/article";
+  //   } catch (error) {
+  //     console.error("Erreur lors de la suppression de l'annonce:", error);
+  //   }
+  // };
 
   return (
     <div className="annonce-detail-container">
@@ -117,12 +116,12 @@ const AnnonceDetail = () => {
           <Link to="/article" className="retour-button">
             Retour
           </Link>
-          {/* Affiche le bouton de suppression si l'annonce a été créée par l'utilisateur actuellement connecté */}
-          {annonce.isCreatedByCurrentUser && (
+
+          {/* {annonce.isCreatedByCurrentUser && (
             <button onClick={() => handleDelete(annonce._id)}>
               Supprimer l'annonce
             </button>
-          )}
+          )} */}
         </div>
       ) : (
         <div>Oups ! Annonce non trouvée.</div>
