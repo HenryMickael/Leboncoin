18a1df5eb3936a55c92e32994d257e42a4df9907
Your Name <henry.mickael1@gmail.com>
2023-09-14 13:32:06 +0200

    test avec fs strem

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,6 @@ app.use((req, res, next) => {
 app.use("/api/auth", userRoutes);
 app.use("/api", vehiculeRoutes);
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+// app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- backend/middleware/multer-configtest.js
+++ backend/middleware/multer-configtest.js
@@ -1,6 +1,29 @@
+// const multer = require("multer");
+// const { MongoClient } = require("mongodb");
+// const GridFsStorage = require("multer-gridfs-storage");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const uri = process.env.MONGODB_URI;
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// const storage = new GridFsStorage({
+//   url: uri,
+//   file: (req, file) => {
+//     return {
+//       filename: file.originalname,
+//     };
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload.single("image");
+
 const multer = require("multer");
 const { MongoClient } = require("mongodb");
-const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -8,14 +31,18 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-const storage = new GridFsStorage({
-  url: uri,
-  file: (req, file) => {
-    return {
-      filename: file.originalname,
-    };
-  },
+let gfs;
+
+client.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à MongoDB :", err);
+    process.exit(1);
+  }
+  gfs = Grid(client.db, require("mongodb"));
+  gfs.collection("images"); // Nom de la collection GridFS
 });
+
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
